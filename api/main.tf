749167d4524b7af8a918754eaa494836a5e0f6e6
mrodriguez <the.mers.17@gmail.com>
2024-12-14 02:30:59 -0600

    v5

--- api/main.tf
+++ api/main.tf
@@ -19,6 +19,10 @@ resource "azurerm_api_management_api" "service_api" {
     content_format = "openapi"
     content_value  = file(var.openapi_spec_path)
   }
+
+  oauth2_authorization {
+    authorization_server_name = "oauth_mers"
+  }
 }
 
 # Esperar a que el API se cree completamente
@@ -81,4 +85,39 @@ resource "azurerm_api_management_backend" "service_backend" {
 data "azurerm_api_management" "existing" {
   name                = split("/", var.apim_id)[8]
   resource_group_name = var.resource_group_name
+}
+
+# CORS Policy
+resource "azurerm_api_management_api_policy" "cors_policy" {
+  api_name            = azurerm_api_management_api.service_api.name
+  api_management_name = split("/", var.apim_id)[8]
+  resource_group_name = var.resource_group_name
+
+  xml_content = <<XML
+<policies>
+    <inbound>
+        <base />
+        <cors>
+            <allowed-origins>
+                <origin>*</origin>
+            </allowed-origins>
+            <allowed-methods>
+                <method>PUT</method>
+            </allowed-methods>
+            <allowed-headers>
+                <header>*</header>
+            </allowed-headers>
+        </cors>
+    </inbound>
+    <backend>
+        <base />
+    </backend>
+    <outbound>
+        <base />
+    </outbound>
+    <on-error>
+        <base />
+    </on-error>
+</policies>
+XML
 }
